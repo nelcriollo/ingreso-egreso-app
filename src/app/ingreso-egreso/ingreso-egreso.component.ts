7f8e90eef3e2eb2d6a3de3f6d448c61973ee43c0
ncriollo <ncriollo@sintad.pe>
2024-12-08 23:57:44 -0500

    primer commit template listo

---
+++ src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ingreso-egreso',
+  templateUrl: './ingreso-egreso.component.html',
+  styleUrl: './ingreso-egreso.component.scss'
+})
+export class IngresoEgresoComponent {
+
+}
